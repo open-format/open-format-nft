cecddd76a0fb65c9f36a679c824664664f53d2c3
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-06-29 14:34:28 +0100

    creating footer component

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Footer from "./footer";
 import Header from "./header";
 
 interface ILayout {
@@ -12,6 +13,7 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
       <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
         <main>{children}</main>
       </div>
+      <Footer />
     </>
   );
 };
