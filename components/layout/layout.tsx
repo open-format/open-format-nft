fe4b2bc3a3ddf98fbcaecdf59336dc81a6960285
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-06-29 15:52:27 +0100

    changing the layout width

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<ILayout> = ({ children }: ILayout) => {
   return (
     <>
       <Header />
-      <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+      <div className="py-6 px-4 sm:px-6 lg:px-8">
         <main>{children}</main>
       </div>
       <Footer />
