9da229a74ddfe1235b4ded562fa8ef750cec7d5a
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-06-29 13:59:53 +0100

    adding the sdk provider around the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout/layout";
+import { OpenFormatProvider } from "@simpleweb/open-format-react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <OpenFormatProvider config={{ network: "mumbai" }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </OpenFormatProvider>
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
