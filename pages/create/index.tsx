64e4b6b07286e5ec28ec55730aa8475a5b67ecca
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-06-30 16:02:29 +0100

    touches to form

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -16,7 +16,7 @@ const Create: NextPage = () => {
                 >
                   <p>Image, Video, or Audio</p>
                   <p className="text-xs text-gray-500">
-                    File types supported: JPEG, PNG, GIF, SVG, MP3 Max 20MB
+                    File types supported: JPEG, PNG MP3
                   </p>
                 </label>
                 <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
@@ -35,6 +35,24 @@ const Create: NextPage = () => {
                         strokeLinejoin="round"
                       />
                     </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-gray-500">
+                      JPEG, PNG, MP3 Max 20MB
+                    </p>
                     <div className="flex text-sm text-gray-600">
                       <label
                         htmlFor="file-upload"
