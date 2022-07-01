3104dda948ce94c2e32cb42b293da09007712808
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-01 11:34:38 +0100

    adjusting blur on image

--- pages/index.tsx
+++ pages/index.tsx
@@ -138,9 +138,9 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
-          <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-white z-0 after:content-['*'] after:absolute after:left-0 after:bottom-0 after:right-0 after:z-10 after:w-full after:h-full after:bg-gradient-to-t after:from-white after:to-white-20">
+          <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-white z-0 after:content-['*'] after:absolute after:left-0 after:bottom-0 after:right-0 after:z-10 after:w-full after:h-full after:bg-gradient-to-t after:from-white after:to-white-20 ">
             <img
-              className="object-cover blur-xl sm:scale-150 md:blur-md"
+              className="object-cover blur-xl sm:scale-150 md:blur-lg"
               src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
               alt=""
             />
