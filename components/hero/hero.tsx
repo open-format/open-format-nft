b13b2ebfe3cfa7decc87d72ada205a6d295eb299
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-04 11:23:46 +0100

    adjusting the backround image component to accept props and be more generic

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -53,7 +53,7 @@ const Hero: React.FC = () => {
           />
         </div>
       </div>
-      <BackgroundImage />
+      <BackgroundImage image="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" />
     </div>
   );
 };
