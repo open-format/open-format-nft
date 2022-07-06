9febb8739303aac3855e8a0c7f8dcd6fb6b7d9f1
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-06 09:25:42 +0100

    additions to the details part of the drop down

--- components/dropdowns/nft-dropdown.tsx
+++ components/dropdowns/nft-dropdown.tsx
@@ -16,13 +16,13 @@ const NFTDropdown: React.FC<NFTDropdownProps> = ({ nftDropdownProps }) => {
   return (
     <div className="border-[1px] mt-4 bg-slate-100 border-slate-200 rounded-lg">
       <div>
-        <p className="text-gray-900 bg-white text-sm font-medium p-6">
+        <p className="text-gray-900 bg-white text-sm font-bold p-6">
           Description
         </p>
       </div>
       <div className="border-t bg-slate-50 border-slate-200">
         <p className="text-gray-900 text-sm font-medium pt-6 pb-2 px-6">
-          By <span className=" font-extrabold">Mr Robot</span>
+          By <span className=" font-extrabold">{name}</span>
         </p>
         <p className="px-6 pb-8 text-sm">{description}</p>
       </div>
@@ -33,8 +33,8 @@ const NFTDropdown: React.FC<NFTDropdownProps> = ({ nftDropdownProps }) => {
             <div className="border-t divide-y border-slate-200">
               <h3>
                 <Disclosure.Button className="group relative w-full p-6 bg-white flex justify-between items-center text-left">
-                  <span className="text-gray-900 text-sm font-medium">
-                    {name}
+                  <span className="text-gray-900 text-sm font-bold">
+                    Details
                   </span>
                   <span className="ml-6 flex items-center">
                     {open ? (
@@ -52,7 +52,32 @@ const NFTDropdown: React.FC<NFTDropdownProps> = ({ nftDropdownProps }) => {
                 </Disclosure.Button>
               </h3>
               <Disclosure.Panel as="div" className="bg-slate-50">
-                <p className="px-6 pb-8 text-sm">{description}</p>
+                <div className="pt-2">
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Contract Address</p>
+                    <p className="px-6 text-sm">0xwe92</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Token ID</p>
+                    <p className="px-6  text-sm">0xwe92</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Token Standard</p>
+                    <p className="px-6 text-sm">ERC-20</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Blockchain</p>
+                    <p className="px-6 text-sm">Polygon</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Metadata</p>
+                    <p className="px-6 text-sm">IPFS</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="px-6 pb-2 text-sm">Creator Fee{"'"}s</p>
+                    <p className="px-6 text-sm">IPFS</p>
+                  </div>
+                </div>
               </Disclosure.Panel>
             </div>
           )}
