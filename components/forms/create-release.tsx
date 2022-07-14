cff139c35d1e14ae3d671d661ad827e423f367e4
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-14 11:56:43 +0100

    fixing the styling here

--- components/forms/create-release.tsx
+++ components/forms/create-release.tsx
@@ -83,6 +83,7 @@ function Dropzone({
           File types supported: JPEG, PNG MP3
         </p>
       </label>
+
       <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
         <div className="space-y-1 text-center">
           <svg
@@ -99,12 +100,20 @@ function Dropzone({
               strokeLinejoin="round"
             />
           </svg>
+          <div className="flex text-sm text-gray-600">
+            <label
+              htmlFor="image"
+              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+            >
+              <span>Upload a file</span>
+              <input {...getInputProps({ onDrop })} />
+            </label>
+            <p className="pl-1">or drag and drop</p>
+          </div>
           <p className="text-xs text-gray-500">JPEG, PNG, MP3 Max 20MB</p>
         </div>
       </div>
-      <div>
-        <input {...getInputProps({ onDrop })} />
-      </div>
+      <div></div>
     </div>
   );
 }
