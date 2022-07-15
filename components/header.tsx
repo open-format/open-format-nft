b0852fa49f92b2dac224f3b985536b482de543c2
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-15 12:04:51 +0100

    all hover states now complete

--- components/header.tsx
+++ components/header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC = () => {
                         setTimeout(() => showTooltip(true), 100);
                       }}
                     >
-                      <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                      <div className="cursor-not-allowed absolute inset-y-0 left-0 pl-3 flex items-center">
                         <SearchIcon
                           className="h-5 w-5 text-gray-400"
                           aria-hidden="true"
