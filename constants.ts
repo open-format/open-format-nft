c4550fb01dba329fb7a124627ddf6362482409a4
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-18 15:36:06 +0100

    footer details complete

---
+++ constants.ts
@@ -0,0 +1,5 @@
+//Footer Links
+export const twitter = "https://twitter.com/simpleweb";
+export const discord = "https://twitter.com/simpleweb";
+export const instagram = "https://www.instagram.com/simplewebhq";
+export const openFormat = "https://openformat.simpleweb.co.uk";
