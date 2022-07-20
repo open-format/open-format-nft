063c9f61c1de67d35db5f6bd55b94fcb1f190e45
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-20 12:43:19 +0100

    resources complete

--- components/resources.tsx
+++ components/resources.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
 import StyledLink from "./styled-link";
 
@@ -6,14 +7,15 @@ interface Props {
 }
 
 export default function Resources({ resources }: Props) {
+  const { t } = useTranslation("common");
   return (
     <div>
       <div className="text-center">
         <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-          Resources
+          {t("resources.title")}
         </h2>
         <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-          Here are some links to help you get started.
+          {t("resources.subTitle")}
         </p>
       </div>
       <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-3xl">
