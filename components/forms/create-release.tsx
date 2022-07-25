346ce422ce8fba9b28f6de60e6cddf03dda93984
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-07-25 12:19:03 +0100

    adjusting the value as per the pr comments

--- components/forms/create-release.tsx
+++ components/forms/create-release.tsx
@@ -275,7 +275,11 @@ export default function CreateReleaseForm() {
                   className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                   defaultValue={t("forms.deploy.defaultValues.blockchain")}
                 >
-                  <option value="polygon">
+                  <option
+                    value={t(
+                      "forms.deploy.labels.blockchain.options.labelOne"
+                    ).toLowerCase()}
+                  >
                     {t("forms.deploy.labels.blockchain.options.valueOne")}
                   </option>
                 </select>
