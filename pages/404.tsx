dcd7475b1a9b829a266c779d007590c1f2db57ef
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-08-02 14:57:26 +0100

    translations complete

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import useTranslation from "next-translate/useTranslation";
 import React from "react";
 
 export default function NotFound() {
-  const { t } = useTranslation();
+  const { t } = useTranslation("common");
   return (
     <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-48 md:grid md:place-items-center lg:px-8">
       <div className="max-w-max mx-auto">
