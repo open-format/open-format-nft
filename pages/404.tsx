97c1d09fa517d5e2d4c2acc6b6d3b8198b4b2fe8
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-08-02 09:57:50 +0100

    404 not found transaltions not working otherwise ready to go

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,23 @@
 import StyledLink from "components/styled-link";
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
 
 export default function NotFound() {
+  const { t } = useTranslation();
   return (
     <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-48 md:grid md:place-items-center lg:px-8">
       <div className="max-w-max mx-auto">
         <main className="sm:flex">
           <p className="text-4xl font-extrabold text-blue-500 sm:text-5xl">
-            404
+            {t("four0four.title")}
           </p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
               <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-                Page not found
+                {t("four0four.subTitle")}
               </h1>
               <p className="mt-1 text-base text-gray-500">
-                Please check the URL in the address bar and try again.
+                {t("four0four.pointerText")}
               </p>
             </div>
             <div className="py-16 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
@@ -25,7 +27,7 @@ export default function NotFound() {
                     href="/explore"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Explore
+                    {t("hero.ctaPrimary")}
                   </StyledLink>
                 </div>
                 <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
@@ -33,7 +35,7 @@ export default function NotFound() {
                     href="/create"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
                   >
-                    Create
+                    {t("hero.ctaSecondary")}
                   </StyledLink>
                 </div>
               </div>
